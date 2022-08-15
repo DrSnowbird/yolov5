5309549197f3bdaeef424761162e10ff904b5a18
DrSnowbird <DrSnowbird@openkbs.org>
2022-08-15 15:04:02 -0400

    updated: test-detect.py: to use CPU as defalt

--- test-detect.py
+++ test-detect.py
@@ -1,7 +1,13 @@
 import torch
 
 # Model
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  # or yolov5n - yolov5x6, custom
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', device='cpu')  # or yolov5n - yolov5x6, custom
+
+#if torch.cuda.is_available():
+#  device = 'cuda'
+#else:
+#  device = 'cpu'
+#model.to(device)
 
 # Images
 img = 'https://ultralytics.com/images/zidane.jpg'  # or file, Path, PIL, OpenCV, numpy, list
